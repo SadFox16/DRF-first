49da4a7de3146ec9acb9354f45757aaf3383cd59
SadFox16 <oleg.s.kerimov@yandex.com>
2022-07-03 00:04:45 +0300

    Start development

---
+++ drfsite/women/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from .models import Women
+
+
+class WomenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Women
+        fields = ('title', 'content', 'cat')
+
+# def encode():
+#     model = WomenModel('Angelina', 'Content: Angelina')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Angelina", "content":"Content: Angelina"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
+
